f8d00bffae490fa07bac7d827ac8d23c38201226
Poojitha-S-004 <poojitha.413@gmail.com>
2026-03-21 19:53:59 +0530

    home page creation

--- smartlearn/urls.py
+++ smartlearn/urls.py
@@ -1,8 +1,15 @@
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/student/', auth_views.LoginView.as_view(template_name='registration/student_login.html'), name='student_login'),
+    path('login/faculty/', auth_views.LoginView.as_view(template_name='registration/faculty_login.html'), name='faculty_login'),
+    
 ]
